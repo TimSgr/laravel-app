a84dd23ce1e4e223813964dc972b3965948b6ed9
Tim <timseeger2000@gmail.com>
2025-02-15 20:30:40 +0000

    breeze installation

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NavbarController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/app', function () {
-    return view('app');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/navbar', [NavbarController::class, 'index']);
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,21 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        './resources/views/**/*.blade.php',
     ],
+
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-            colors: {
-                darkpurple: "#1E152A",
-                error: "#EF2D56",
-                white: "#FFFFFF",
-                success: "#7BD389",
-                warning: "#FFFC31",
-                text: "#182825",
-                blue: "#3D3B8E",
-                darkblue: "#24234C",
-                lightblue: "#9AC2F6", 
-                newtext: "#F6F8FD",
-                newtext2: "#EDF0FB",
-                tierheimText: "#009034",
-                tierheimMenuBg: "#00481A",
-                paulaidee: "#CEDCB2",
-                paulaundjasminidee: "#00240D",
-              },
         },
     },
-    plugins: [],
+
+    plugins: [forms],
 };
