c4986f5ac79dd6990b7f3d6d36812f3b3fe37cf1
Tim <timseeger2000@gmail.com>
2025-02-16 12:26:35 +0000

    fix login issue

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,8 +12,14 @@
         <!-- Name -->
         <div class="mt-4">
             <x-input-label for="last_name" :value="__('Last Name')" />
-            <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autofocus autocomplete="name" />
+            <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autofocus autocomplete="last_name" />
             <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',  // Wichtig für externen Zugriff
+        port: 5173,        // Standardport für Vite
+        strictPort: true,
+        hmr: {
+            host: '192.168.178.68'  // IP deines Raspberry Pis im Netzwerk
+        }
+    }
 });
